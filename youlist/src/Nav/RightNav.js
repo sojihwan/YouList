0cf12c3dffaee1e14652742f18f643097dd1f5b5
sojihwan <73238543+sojihwan@users.noreply.github.com>
2021-11-26 14:59:35 +0900

    오른Nav

--- youlist/src/Nav/RightNav.js
+++ youlist/src/Nav/RightNav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Link } from 'react-router-dom';
 
 function Nav (){
   return(
@@ -9,15 +10,16 @@ function Nav (){
         <br/>미완성
         <br/>
         <select multiple className="form-control">
-          <option>추천</option>
           <option>웃긴</option>
           <option>음악</option>
           <option>애니메이션</option>
           <option>감상</option>
           <option>게임</option>
+          <option>공부</option>
         </select>
         <strong>업로드</strong>
         <br/>
+        <Link to="/uplode" style={{textDecoration:"none",color:"#000000"}}>
         <select multiple className="form-control">
           <option>웃긴-업로드</option>
           <option>음악-업로드</option>
@@ -25,6 +27,7 @@ function Nav (){
           <option>감상-업로드</option>
           <option>게임-업로드</option>
         </select>
+        </Link>
         </h1>
     </nav>
   )
