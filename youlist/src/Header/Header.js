522da99ef3ca1dcd35d4354d2a2eb0c2d022bddf
sojihwan <73238543+sojihwan@users.noreply.github.com>
2021-11-26 15:00:19 +0900

    머리

--- youlist/src/Header/Header.js
+++ youlist/src/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Link } from 'react-router-dom';
 
 function Header (){
   return(
@@ -11,7 +12,7 @@ function Header (){
         </h1>
         <div>
         <button type="button" className="btn btn-success"style={{background:'#2F4F4F' ,width:'100%'}}>join</button><br/>
-        <button type="button" className="btn btn-warning"style={{background:'#2F4F4F' ,width:'100%'}}>log-in</button><br/>
+        <button type="button" className="btn btn-warning"style={{background:'#2F4F4F' ,width:'100%'}}><Link to="/LogIn" style={{textDecoration:"none",color:"#000000"}}>log-in</Link></button><br/>
         <button type="button" className="btn btn-danger"style={{background:'#2F4F4F' ,width:'100%'}}>log-out</button><br/>
         </div>
     </header>
