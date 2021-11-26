f45dc228e03f57e529adbb8d8ecb134bf46e375b
sojihwan <73238543+sojihwan@users.noreply.github.com>
2021-11-26 15:00:55 +0900

    몸통

--- youlist/src/Body/Body.js
+++ youlist/src/Body/Body.js
@@ -2,18 +2,21 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css';
 
+let num=0;
+
 function Bady (){
+  function onClick() {
+    num=num+1;
+  }
   return(
     <bady style={{background:'#2F4F4F',width:'70%',height:'550px',position:'absolute',top:'220px',left:'30%'}} className="center-block">
       <table className="table table-striped">
         <th>이름</th>
         <th>조회수</th>
-        <th>댓글</th>
         <th>태그</th>
         <tr>
-          <td><Link to="/View" style={{textDecoration:"none",color:"#000000"}}>React 기초 0강 : 리액트왜 쓰는지 알려줌 (+ 수강시 필요 사전지식)</Link></td>
-          <td>63,531회</td>
-          <td>xx회</td>
+          <td onClick={onClick}><Link to="/View" style={{textDecoration:"none",color:"#000000"}}>React 기초 0강 : 리액트왜 쓰는지 알려줌 (+ 수강시 필요 사전지식)</Link></td>
+          <td>{num}회</td>
           <td>#공부 #리엑트</td>
         </tr>
         <tr>
