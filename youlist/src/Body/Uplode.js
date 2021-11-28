0c2c2c221017805dd6654999da791a5eca080d32
sojihwan <73238543+sojihwan@users.noreply.github.com>
2021-11-28 16:01:59 +0900

    11/28

--- youlist/src/Body/Uplode.js
+++ youlist/src/Body/Uplode.js
@@ -1,8 +1,7 @@
 import React from "react";
 
 function Uplode() {
-  function login(){
-  }
+  
   return(
     <bady style={{background:'#2F4F4F',width:'60%',height:'550px',position:'absolute',top:'220px',left:'30%'}} className="center-block">
       <h3>제목</h3><br/>
@@ -11,7 +10,7 @@ function Uplode() {
       <input type="text"style={{width:"90%"}} required/><br/>
       <h3>태그</h3><br/>
       <input type="text"style={{width:"90%"}} required/><br/>
-      <button className="btn btn-warning" style={{width:"100px",height:"50px",background:'#2F4F4F'}} onClick={login}>올리기</button>
+      <button className="btn btn-warning" style={{width:"100px",height:"50px",background:'#2F4F4F'}}s>올리기</button>
     </bady>
   )
 }
--- youlist/src/LogIn/LogInPage.js
+++ youlist/src/LogIn/LogInPage.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React,{useState} from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 
 
 function Modal (){
+  const [Log,setLog] = useState({
+    id: "",
+    pw: ""
+  });
+  
+  const {id ,pw} =Log
+
+  function iog(){
+    
+    if(Log===" "){
+      alert("비밀번호 또는 아이디을 입력해주세요")
+    }
+  }
+
   return(
     <bady style={{background:'#2F4F4F',width:'70%',height:'550px',position:'fixed',top:'220px',left:'30%'}} className="center-block">
           <p style={{position:"absolute",top:"25%",left:"20%",width:"40%",height:"30%",backgroundColor:"#DCDCDC"}}>
             <p style={{position:"absolute",top:"15%",left:"20%"}}>
               ID<br/>
-              <input type="text"/>
+              <input name="id" type="text" value="id"/>
               <br/> 
               PW<br/>
-              <input type="password"/>
+              <input name="pw" type="password" value="pw"/>
             </p>
-            <button style={{position:"absolute",top:"25%",left:"65%",width:"20%",height:"60%"}}>LONIN</button>
+            <button style={{position:"absolute",top:"25%",left:"65%",width:"20%",height:"60%"}} onClick={iog}>LONIN</button>
           </p>
     </bady>
   )
