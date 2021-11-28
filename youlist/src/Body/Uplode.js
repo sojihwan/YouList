29cc5e53b215811ada42728f28177034365eb8f8
sojihwan <73238543+sojihwan@users.noreply.github.com>
2021-11-28 15:46:18 +0900

    11/28

--- youlist/src/Body/Uplode.js
+++ youlist/src/Body/Uplode.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 function Uplode() {
+  function login(){
+  }
   return(
     <bady style={{background:'#2F4F4F',width:'60%',height:'550px',position:'absolute',top:'220px',left:'30%'}} className="center-block">
       <h3>제목</h3><br/>
@@ -9,7 +11,7 @@ function Uplode() {
       <input type="text"style={{width:"90%"}} required/><br/>
       <h3>태그</h3><br/>
       <input type="text"style={{width:"90%"}} required/><br/>
-      <button className="btn btn-warning" style={{width:"100px",height:"50px",background:'#2F4F4F'}}>올리기</button>
+      <button className="btn btn-warning" style={{width:"100px",height:"50px",background:'#2F4F4F'}} onClick={login}>올리기</button>
     </bady>
   )
 }
