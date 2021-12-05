065a77a8f0c8ceab6aeee99bf303e7ad6ebd486f
sojihwan <73238543+sojihwan@users.noreply.github.com>
2021-12-05 20:11:35 +0900

    12/5

--- youlist/src/Body/Body.js
+++ youlist/src/Body/Body.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {Link} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css';
 
+
 function Listadd({listnum},props){
   return(
     <tr>
