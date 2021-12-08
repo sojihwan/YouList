377e5e419d9e9d5ff6468d99a88151a2567ae5e4
sojihwan <73238543+sojihwan@users.noreply.github.com>
2021-12-08 14:07:18 +0900

    12/08

--- youlist/src/Body/Listadd.js
+++ youlist/src/Body/Listadd.js
@@ -1,18 +1,14 @@
+/* eslint-disable no-sequences */
 import React from 'react';
 import {Link} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css';
 
 
-function Listadd({listnum},props){
-  function ls(){
-      console.log(listnum)
-      const ls=listnum
-      console.log(ls)
-  }
+function Listadd({listnum,onRemomve},props){
   return(
     <tr>
       <td onClick={() => props.setData(`${listnum.like}`)}><Link to="/View" style={{textDecoration:"none",color:"#FFFFFF"}}>{listnum.title}</Link></td>
-      <td><button className='btn btn-default' style={{color:'#FFFFFF'}} onClick={ls}>삭제</button></td>
+      <td><button className='btn btn-default' style={{color:'#FFFFFF',width:'60px'}} onClick={()=>onRemomve(listnum.like)}><Link to="/" style={{textDecoration:"none",color:"#FFFFFF"}}>삭제</Link></button></td>
       <td style={{color:'#FFFFFF'}}>{listnum.tag}</td>
     </tr>
   );
