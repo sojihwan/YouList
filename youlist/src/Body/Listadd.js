6a4fddb6141fe850c1d82a5376c68a2ff6eb922a
sojihwan <73238543+sojihwan@users.noreply.github.com>
2021-12-08 09:29:31 +0900

    12/08

--- youlist/src/Body/Listadd.js
+++ youlist/src/Body/Listadd.js
@@ -4,14 +4,15 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 
 function Listadd({listnum},props){
-  function aaaa(){
-      console.log(props.setData)
+  function ls(){
       console.log(listnum)
+      const ls=listnum
+      console.log(ls)
   }
   return(
     <tr>
       <td onClick={() => props.setData(`${listnum.like}`)}><Link to="/View" style={{textDecoration:"none",color:"#FFFFFF"}}>{listnum.title}</Link></td>
-      <td><button className='btn btn-default' style={{color:'#FFFFFF'}} onClick={aaaa}>삭제</button></td>
+      <td><button className='btn btn-default' style={{color:'#FFFFFF'}} onClick={ls}>삭제</button></td>
       <td style={{color:'#FFFFFF'}}>{listnum.tag}</td>
     </tr>
   );
--- youlist/src/Nav/RightNav.js
+++ youlist/src/Nav/RightNav.js
@@ -3,35 +3,27 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { Link } from 'react-router-dom';
 
 function RightNav (){
-  const [Log, setLog] = useState({
-    selectop:''
-  });
-  const {selectop}  = Log;
+  const [text, setText] = useState('');
 
   const onChange = (e) => {
-    const { name, value } = e.target;
-    setLog({
-      ...Log,
-      [name]: value,
-    });
-    console.log(selectop);
+    setText(e.target.value);
   };
   return(
     <nav style={{padding:"5px",background:'#DCDCDC',width:'20%',height:'550px',borderRadius:"20PX"}} className="center-block">
       <h1 style={{textAlign:'center'}}>
         <strong>태그</strong>
         <br/>
-          {selectop}
+          {text}
         <br/>
-        <select className="btn btn-secondary dropdown-toggle" onChange={onChange} value={selectop} style={{textAlign:"center"}}>
-          <option value="추천" name="1" >추천</option>
-          <option value="웃긴" name="2" >웃긴</option>
-          <option value="음악" name="3" >음악</option>
-          <option value="애니메이션" name="4" >애니메이션</option>
-          <option value="감상" name="5" >감상</option>
-          <option value="게임" name="6" >게임</option>
-          <option value="공부" name="7" >공부</option>
-          <option value="힐링" name="8" >힐링</option>
+        <select className="btn btn-secondary dropdown-toggle" onChange={onChange} value={text} style={{textAlign:"center"}}>
+          <option value="추천"  >추천</option>
+          <option value="웃긴"  >웃긴</option>
+          <option value="음악"  >음악</option>
+          <option value="애니메이션"  >애니메이션</option>
+          <option value="감상"  >감상</option>
+          <option value="게임"  >게임</option>
+          <option value="공부"  >공부</option>
+          <option value="힐링"  >힐링</option>
         </select><br/>
         <strong>업로드</strong>
         <br/>
