1535f2b18d54f42e358479d70a26316bd5ede01d
sojihwan <73238543+sojihwan@users.noreply.github.com>
2021-12-09 17:58:31 +0900

    12/09

--- youlist/src/Body/Listadd.js
+++ youlist/src/Body/Listadd.js
@@ -2,15 +2,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css';
-
+import '../CSS/Link.css'
 
 function Listadd({listnum,onRemomve,onView}){
   return(
     <tr>
-      <td onClick={() => onView(listnum.like)}><Link to="/View" style={{textDecoration:"none",color:"#FFFFFF"}}>{listnum.title}</Link></td>
+      <td onClick={() => onView(listnum.like)}><Link to="/View" className="Link">{listnum.title}</Link></td>
       <td><button className='btn btn-default' style={{color:'#FFFFFF',width:'60px'}} onClick={()=>onRemomve(listnum.like)}><Link to="/" style={{textDecoration:"none",color:"#FFFFFF"}}>삭제</Link></button></td>
       <td style={{color:'#FFFFFF'}}>{listnum.tag}</td>
     </tr>
   );
 }
-export default Listadd
+export default React.memo(Listadd)
