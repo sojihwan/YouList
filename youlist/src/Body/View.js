20f6daaa587032313a0241e07fd2ee60cbf02e2a
sojihwan <73238543+sojihwan@users.noreply.github.com>
2021-12-09 17:58:40 +0900

    12/09

--- youlist/src/Body/View.js
+++ youlist/src/Body/View.js
@@ -1,10 +1,11 @@
+import '../CSS/Div.css'
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function View (props){
   const like =`https://www.youtube.com/embed/${props.name}`
   return(
-    <div style={{padding:"5px",background:'#000000',width:'70%',height:'550px',position:'absolute',top:'0px',left:'15%'}} className="center-block">
+    <div className='DIVVIEW'>
 
      <iframe width="100%" height="100%" src={like} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
