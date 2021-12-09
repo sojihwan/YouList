452d60ea97b65cb21fa81ecbfcd4e4cc256320ea
sojihwan <73238543+sojihwan@users.noreply.github.com>
2021-12-09 17:59:06 +0900

    12/09

--- youlist/src/Nav/LeftNav.js
+++ youlist/src/Nav/LeftNav.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Link } from 'react-router-dom';
+import '../CSS/Div.css'
+import '../CSS/Link.css'
 
 function LeftNav (){
   return(
-    <nav style={{borderRadius:"20PX",background:'#DCDCDC',width:'15%',height:'550px',position:'absolute',left:'85%',top:'0px'}} className="center-block">
-      <h2 style={{textAlign:'center'}}>
+    <nav className="LeftNav">
+      <h2 className='LeftH1'>
         <strong>이동</strong>
         <p>
         <br/>
@@ -13,7 +15,7 @@ function LeftNav (){
         영상이름<br/>
         <br/>
         </p>
-        <Link to="/" style={{textDecoration:"none",color:"#000000"}}>리스트로</Link><br/>
+        <Link to="/"className="Link1">리스트로</Link><br/>
         <br/>
         <p>
         이전 영상<br/>
