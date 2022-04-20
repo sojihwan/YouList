c2552986e5e733cb86129fea1ac4ac2babd8cd0b
sojihwan <73238543+sojihwan@users.noreply.github.com>
2022-04-20 17:45:34 +0900

    4/20

--- youlist/src/Body/Body.js
+++ youlist/src/Body/Body.js
@@ -1,40 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Listadd from './Listadd';
 import '../CSS/Div.css'
 import '../CSS/Link.css'
 
-function Body (){
-  const [lists,setLists] =useState([
+function Body() {
+  const [lists, setLists] = useState([
   ])
-  const addlist = () =>{
-    for (var i = 0; i < localStorage.length; i++){
-      setLists([...lists,JSON.parse(localStorage.getItem(`${i}`))]);
+  const addlist = () => {
+    for (var i = 0; i < localStorage.length; i++) {
+      setLists([...lists, JSON.parse(localStorage.getItem(`${i}`))]);
     }
   }
-  
-    const onRemomve = like=>{
+  useEffect(() => {
+    for (var i = 0; i < localStorage.length; i++) {
+      const data = JSON.parse(localStorage.getItem(`${i}`))
+      if (data) {
+        setLists(data);
+      }
+    }
+  }, []);
+
+  const onRemomve = like => {
     setLists(lists.filter(lists => lists.like !== like));
     console.log(lists);
   }
-  return(
+  return (
     <div className="Bodydiv">
-        <table className="table table-striped" style={{color:"#FFFFFF"}} name="list1" >
-          <thead>
-            <tr>
-              <th>이름</th>
-              <th>삭제</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              lists.map(
-                listnum => (<Listadd listnum={listnum} key={listnum.like} onRemomve={onRemomve}/>)
-              )
-            }
-          </tbody>
-        </table>
-        <button className="btn btn-success"onClick={addlist} style={{backgroundColor:"#000000"}}>reload</button>
+      <table className="table table-striped" style={{ color: "#FFFFFF" }} name="list1" >
+        <thead>
+          <tr>
+            <th>이름</th>
+            <th>삭제</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            lists.map(
+              listnum => (<Listadd listnum={listnum} key={listnum.like} onRemomve={onRemomve} />)
+            )
+          }
+        </tbody>
+      </table>
+      <button className="btn btn-success" onClick={addlist} style={{ backgroundColor: "#000000" }}>reload</button>
     </div>
   )
 }
