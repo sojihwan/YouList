0fd82ce85913396e3acc0b2cc44e80979c873dc5
sojihwan <73238543+sojihwan@users.noreply.github.com>
2022-05-16 14:57:40 +0900

    5/16

--- youlist/src/Body/Listadd.js
+++ youlist/src/Body/Listadd.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable no-sequences */
 import React from 'react';
 import {Link} from 'react-router-dom'
@@ -5,9 +6,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../CSS/Link.css'
 
 function Listadd({listnum,onRemomve},props){
+  const like = () =>{
+    props.getlike(listnum)//자식에서 부모로 값보내기
+  }
   return(
     <tr>
-      <td><Link to="/View" className="Link">{listnum.title} </Link></td>
+      <td><Link to="/View" className="Link"><a onClick={like}>{listnum.title}</a></Link></td>
       <td><button className='btn btn-default' style={{color:'#FFFFFF',width:'60px'}} onClick={()=>onRemomve(listnum.like)}><Link to="/" style={{textDecoration:"none",color:"#FFFFFF"}}>삭제</Link></button></td>
     </tr>
   );
