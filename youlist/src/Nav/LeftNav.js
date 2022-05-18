9f080a120e7a6496cc86270cc487520c954a8565
sojihwan <73238543+sojihwan@users.noreply.github.com>
2022-05-18 10:48:31 +0900

    5/18

--- youlist/src/Nav/LeftNav.js
+++ youlist/src/Nav/LeftNav.js
@@ -3,27 +3,25 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { Link } from 'react-router-dom';
 import '../CSS/Div.css'
 import '../CSS/Link.css'
+import Body from '../Body/Body';
 
 function LeftNav (props){
-  function next(){
-
-  }
+  
   return(
     <nav className="LeftNav">
       <h2 className='LeftH1'>
         <strong>이동</strong>
-        <p>
-        <br/>
-        <b>다음 영상</b><br/>
-        neitem.title<br/>
-        <br/>
-        </p>
-        <Link to="/"className="Link1">리스트로</Link><br/>
-        <br/>
-        <p>
-        <b>이전 영상</b><br/>
-        beitem.title<br/>
-        </p>
+        <div className='LeftNavDiv'>
+          <Link to="/"className="Link1"><b>리스트로</b></Link><br/>
+        </div>
+        <div className='LeftNavDiv'>
+          <b onClick={Body.beforepage}>이전</b><br/>
+          <b onClick={Body.beforepage}>페이지</b>
+        </div>
+        <div className='LeftNavDiv'>
+          <b onClick={Body.nextpage}>다음</b><br/>
+          <b onClick={Body.nextpage}>페이지</b>
+        </div>
         </h2>
     </nav>
   )
