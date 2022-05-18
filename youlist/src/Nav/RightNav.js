21243052803e06d1bcb3a4cf8c594261bdb59af3
sojihwan <73238543+sojihwan@users.noreply.github.com>
2022-05-18 10:48:17 +0900

    5/18

--- youlist/src/Nav/RightNav.js
+++ youlist/src/Nav/RightNav.js
@@ -10,14 +10,22 @@ function RightNav (){
     <nav className="RightNav">
       <h1 style={{textAlign:'center'}}>
         <br/>
-        <strong>업로드</strong>
-        <br/>
         <Link to="/uplode"className="Link1">
-        <select className="btn btn-secondary dropdown-toggle"style={{textAlign:"center"}}>
-          <option>업로드</option>
-        </select>
+        <strong>업로드</strong>
         </Link>
         <br/>
+        <br/>
+        <br/>
+        <br/> 
+        <div>
+          <b>페이지<br/>이동</b>
+        <select className="btn btn-secondary dropdown-toggle"style={{textAlign:"center"}}>
+          <option value={1}>1페이지</option>
+          <option value={2}>2페이지</option>
+          <option value={3}>3페이지</option>
+          <option value={4}>4페이지</option>
+        </select>
+        </div>
         </h1>
     </nav>
   )
