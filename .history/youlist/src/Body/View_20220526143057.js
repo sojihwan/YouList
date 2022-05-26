54bd768831107cfe2f9b54be45888d74ce8da907
sojihwan <73238543+sojihwan@users.noreply.github.com>
2022-05-26 15:05:26 +0900

    5/26

---
+++ .history/youlist/src/Body/View_20220526143057.js
@@ -0,0 +1,17 @@
+import '../CSS/Div.css'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function View (props){
+  console.log(props)
+  let an = 0  
+  const value = JSON.parse(localStorage.getItem(`${props.value+an}`));
+  const like =`https://www.youtube.com/embed/${value.like}`
+  return(
+    <div className='DIVVIEW'>
+
+     <iframe width="100%" height="100%" src={like} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    </div>
+  )
+}
+export default View;
